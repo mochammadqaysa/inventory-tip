79bef95c1ce7b96a759bfca38a85a1ffe058213e
mochammadqaysa <qaysa78@gmail.com>
2024-09-09 13:54:17 +0700

    [UPDATE] service, about page

--- resources/views/pages/landing_page/services.blade.php
+++ resources/views/pages/landing_page/services.blade.php
@@ -189,7 +189,7 @@
                 <div class="col-md-5">
                     <h3>Ready to order your project ? </h3>
                 </div>
-                <div class="col-md-3"><a href="contact.html" class="btn float-btn flat-btn">Get in Touch</a></div>
+                <div class="col-md-3"><a href="{{route('landing.contacts')}}" class="btn float-btn flat-btn">Get in Touch</a></div>
             </div>
         </div>
     </div>
